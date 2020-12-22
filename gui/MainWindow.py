b2c92b127d53578a6621c5dc9037ba310fdef37d
tkongy <277250394@qq.com>
2020-12-22 16:50:07 +0800

    update video

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -3,7 +3,6 @@ import os
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from gui.video_gui import vedio
 from gui.picture_gui import runimage
 from gui.cameraset import Camera
 import os
